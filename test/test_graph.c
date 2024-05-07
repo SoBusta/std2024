c68075f1cb708342e2d390279871d91b6cc4a7d5
Brian Pulfer <Brian.Pulfer@unige.ch>
2024-05-07 12:52:54 +0200

    Extra tests TP4

--- test/test_graph.c
+++ test/test_graph.c
@@ -27,7 +27,32 @@ void dijkstra(){
     graph_destroy(graph);
 }
 
+void dijkstra_extra(){
+    Graph* graph = graph_create(5);
+    graph_add_edge(graph, 0, 1, 1);
+    graph_add_edge(graph, 0, 2, 4);
+    graph_add_edge(graph, 0, 3, 5);
+    graph_add_edge(graph, 1, 2, 2);
+    graph_add_edge(graph, 1, 3, 7);
+    graph_add_edge(graph, 2, 3, 1);
+    graph_add_edge(graph, 2, 4, 3);
+    graph_add_edge(graph, 3, 4, 2);
+    TEST_ASSERT_(graph_dijkstra(graph, 0, 1) == 1, "dijkstra(graph, 0, 1) == 1 failed");
+    TEST_ASSERT_(graph_dijkstra(graph, 0, 2) == 3, "dijkstra(graph, 0, 2) == 3 failed");
+    TEST_ASSERT_(graph_dijkstra(graph, 0, 3) == 4, "dijkstra(graph, 0, 3) == 4 failed");
+    TEST_ASSERT_(graph_dijkstra(graph, 0, 4) == 6, "dijkstra(graph, 0, 4) ==  failed");
+    TEST_ASSERT_(graph_dijkstra(graph, 1, 2) == 2, "dijkstra(graph, 1, 2) == 2 failed");
+    TEST_ASSERT_(graph_dijkstra(graph, 1, 3) == 3, "dijkstra(graph, 1, 3) == 3 failed");
+    TEST_ASSERT_(graph_dijkstra(graph, 1, 4) == 5, "dijkstra(graph, 1, 4) == 5 failed");
+    TEST_ASSERT_(graph_dijkstra(graph, 2, 3) == 1, "dijkstra(graph, 2, 3) == 1 failed");
+    TEST_ASSERT_(graph_dijkstra(graph, 2, 4) == 3, "dijkstra(graph, 2, 4) == 3 failed");
+    TEST_ASSERT_(graph_dijkstra(graph, 3, 4) == 2, "dijkstra(graph, 3, 4) == 2 failed");
+
+    graph_destroy(graph);
+}
+
 TEST_LIST = {
     { "dijkstra", dijkstra },
+    { "dijkstra_extra", dijkstra_extra},
     { NULL, NULL }
 };
