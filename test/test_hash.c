3a448acea5d92a15cde509e13df17f6bd9f18640
Brian Pulfer <Brian.Pulfer@unige.ch>
2024-05-21 11:43:20 +0200

    Extra tests TP5

--- test/test_hash.c
+++ test/test_hash.c
@@ -1,10 +1,9 @@
 #include "acutest.h"
 #include "./../lib/hash/hash.h"
 
-void hash_function()
-{
-    TEST_CHECK_(hashFunction("chat") == 352407, "Error: hashFunction");
-    TEST_CHECK_(hashFunction("g") == 7, "Error: hashFunction");
+void hash_function(){
+    char* a = "ba";
+    TEST_CHECK_(hashFunction(a) == 28, "Error: hashFunction");
 }
 
 void regular_hash_insert_and_get()
@@ -46,10 +45,51 @@ void chain_hash_delete()
     TEST_ASSERT_(hash_chain_get(table, (void *)"g") == NULL, "Error: remove");
 }
 
+void extra_regular_hash_insert_and_get(){
+    HashTable *table = hashCreate(5, (unsigned long int (*)(void *)) & hashFunction);
+    hash_insert(table, (void *)"chat", (void *)1);
+    TEST_ASSERT_(hash_get(table, (void *)"chat") == (void *)1, "Error: insert");
+    hash_insert(table, (void *)"chat", (void *)2);
+    TEST_ASSERT_(hash_get(table, (void *)"chat") == (void *)2, "Error: insert");
+}
+
+void extra_regular_hash_delete(){
+    HashTable *table = hashCreate(5, (unsigned long int (*)(void *)) & hashFunction);
+    hash_insert(table, (void *)"chat", (void *)1);
+    hash_insert(table, (void *)"chat", (void *)2);
+    hash_delete(table, (void *)"chat");
+    TEST_ASSERT_(hash_get(table, (void *)"chat") == NULL, "Error: remove");
+}
+
+void extra_chain_hash_insert_and_get(){
+    HashTableChain *table = hashChainCreate(5, (unsigned long int (*)(void *)) & hashFunction);
+    hash_chain_insert(table, (void *)"chat", (void *)1);
+    TEST_ASSERT_(hash_chain_get(table, (void *)"chat") == (void *)1, "Error: insert");
+    hash_chain_insert(table, (void *)"g", (void *)2);
+    TEST_ASSERT_(hash_chain_get(table, (void *)"chat") == (void *)1, "Error: insert");
+    TEST_ASSERT_(hash_chain_get(table, (void *)"g") == (void *)2, "Error: insert");
+}
+
+void extra_chain_hash_delete(){
+    HashTableChain *table = hashChainCreate(5, (unsigned long int (*)(void *)) & hashFunction);
+    hash_chain_insert(table, (void *)"chat", (void *)1);
+    hash_chain_insert(table, (void *)"g", (void *)2);
+    hash_chain_delete(table, (void *)"chat");
+    TEST_ASSERT_(hash_chain_get(table, (void *)"chat") == NULL, "Error: remove");
+    TEST_ASSERT_(hash_chain_get(table, (void *)"g") == (void *)2, "Error: remove");
+    hash_chain_delete(table, (void *)"g");
+    TEST_ASSERT_(hash_chain_get(table, (void *)"g") == NULL, "Error: remove");
+}
+
 TEST_LIST = {
-    {"hash_function", hash_function},
+    { "hash_function", hash_function },
     {"regular_hash_insert_and_get", regular_hash_insert_and_get},
     {"regular_hash_delete", regular_hash_delete},
     {"chain_hash_insert_and_get", chain_hash_insert_and_get},
     {"chain_hash_delete", chain_hash_delete},
-    {NULL, NULL}};
+    { "extra_regular_hash_insert_and_get", extra_regular_hash_insert_and_get },
+    { "extra_regular_hash_delete", extra_regular_hash_delete },
+    { "extra_chain_hash_insert_and_get", extra_chain_hash_insert_and_get },
+    { "extra_chain_hash_delete", extra_chain_hash_delete },
+    { NULL, NULL }
+};
